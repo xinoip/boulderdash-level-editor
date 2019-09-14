44e471cb368f906b76cddc3abaa522794701003d
xinoip <xinoipionix@gmail.com>
2019-09-15 01:26:42 +0300

    isDigit input check

--- inputChecker.c
+++ inputChecker.c
@@ -1,15 +1,36 @@
 #include "./inputChecker.h"
 
-int isDigit(SDL_Event e) {
-    switch(e.key.keysym.sym) {
-        case SDLK_1:
-            printf("ITS HOTDOG\n");
+int isDigit(SDL_Event e)
+{
+    int rtr = 0;
+    switch (e.key.keysym.sym)
+    {
+    case SDLK_0:
+    case SDLK_1:
+    case SDLK_2:
+    case SDLK_3:
+    case SDLK_4:
+    case SDLK_5:
+    case SDLK_6:
+    case SDLK_7:
+    case SDLK_8:
+    case SDLK_9:
+    case SDLK_KP_0:
+    case SDLK_KP_1:
+    case SDLK_KP_2:
+    case SDLK_KP_3:
+    case SDLK_KP_4:
+    case SDLK_KP_5:
+    case SDLK_KP_6:
+    case SDLK_KP_7:
+    case SDLK_KP_8:
+    case SDLK_KP_9:
+        rtr = 1;
         break;
-        default:
-        printf("NOT HOTDOG\n");
+    default:
+        rtr = 0;
         break;
     }
-    
-    
-    return 1;
+
+    return rtr;
 }
